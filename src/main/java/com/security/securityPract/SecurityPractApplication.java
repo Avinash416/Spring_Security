64a5d12def174739edd354cbc5c826c3d7fc8d8d
Avinash416 <avijadhav416@gmail.com>
2025-04-19 22:19:26 +0530

    feat: Initilaize project with spring security dependency

---
+++ src/main/java/com/security/securityPract/SecurityPractApplication.java
@@ -0,0 +1,13 @@
+package com.security.securityPract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityPractApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityPractApplication.class, args);
+	}
+
+}
